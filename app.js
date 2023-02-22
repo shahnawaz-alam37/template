bcd5526b5f6ff75652c95419c38a5772fa8eef10
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2023-02-22 19:54:44 +0530

    added more html files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 // const mysql = require('mysql'); use this if your are using a mysql database
 // const mongoose = require('mongoose'); use this if your are using a mongoose database
 
-const studentmodel = require('/models/student');
+// const studentmodel = require('models/student');
 // importing the model of the student from models directory
 
 
@@ -38,12 +38,21 @@ app.use(bodyParser.json());
 
 // Define routes
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Student Database Management' });
+    res.render('index', { title: 'Student Database Management' });
 });
 
-app.get('/next',(req, res) => {
-    res.render('next', { title: 'next page', uname:'new page' });
+app.get('/services',(req, res) => {
+    res.render('services', { title: 'services'});
 });
+
+app.get('/aboutus',(req, res) => {
+    res.render('aboutus', { title: 'about us'});
+});
+
+app.get('/admin',(req, res) => {
+    res.render('admin', { title: 'Admin'});
+});
+
 
 // Start the server on localhost at port 3000
 const port = process.env.PORT || 3000;
